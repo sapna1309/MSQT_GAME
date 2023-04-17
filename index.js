4aa199e1a8eb4b81cfedd16165fec136688ba96b
Sapna Sharma <sapna.sharma220516@gmail.com>
2023-04-17 14:48:34 +0530

    mosquito

---
+++ index.js
@@ -0,0 +1,31 @@
+let mosquito = document.querySelector(".mosquito-img");
+let gameSatus = document.querySelector(".game-status")
+let isGameStarted=false;
+let hitCount=0;
+
+const startGame=()=>{    
+ isGameStarted=true;
+ hitCount=0;
+ document.querySelector(".total").innerText=hitCount
+ gameSatus.innerText="GAME STARTED 🎉";
+ let id=setInterval(()=>{
+ gameSatus.innerText="";
+ let i = Math.floor(Math.random()*90)+1;
+ let j = Math.floor(Math.random()*90)+1;
+ mosquito.style.left = i+"%";
+ mosquito.style.top = j+"%";
+},1000);
+
+setTimeout(()=>{
+    clearTimeout(id);
+    isGameStarted=false;
+    gameSatus.innerText="GAME OVER 😯";
+},30000)
+}
+
+const hitMosquito=()=>{
+ if(isGameStarted){
+ hitCount++;
+ document.querySelector(".total").innerText=hitCount;
+ }
+}
